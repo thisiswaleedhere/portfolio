c3654f4b2539da722298a46701db3c5db5b492d0
thisiswaleedhere <thisiswaleedhere@gmail.com>
2022-07-15 22:12:30 +0400

    made responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,42 @@
+import { ReactComponent as LogoMW } from "../images/LOGOMW.svg";
+import { useEffect, useState } from "react";
+
 const Footer = () => {
+
+    const [copySuccess, setCopySuccess] = useState(null);
+
+    useEffect(() => {
+        setTimeout(() => { setCopySuccess(null) }, 4000)
+    }, [copySuccess])
+
+
+    const copyToClipBoard = async copyMe => {
+        try {
+            await navigator.clipboard.writeText(copyMe);
+            setCopySuccess('Email Copied!');
+        }
+        catch (err) {
+            setCopySuccess('Failed to copy!');
+        };
+    }
+
+
     return (
-        <footer className="p-12 flex justify-between font-ubuntu max-w-7xl w-4/5 mx-auto">
-            <div>Made by MW</div>
-            <div className="flex space-x-8 text-indigo-500 font-semibold">
-                <div>EMAIL</div>
-                <div>GITHUB</div>
+        <footer className="md:p-12 pt-12 pb-6 flex flex-col sm:flex-row justify-between font-ubuntu max-w-7xl w-4/5 mx-auto min-w-[350px]">
+
+            <div className="mx-auto sm:mx-0 flex space-x-3 divide-x divide-gray-600 align-middle text-gray-700">
+                <LogoMW className="w-8" />
+
+                <div className="pt-1 pl-2">Made by MW</div>
             </div>
+
+            {copySuccess && <div className="text-sm text-pink-500">{copySuccess}</div>}
+
+            <div className="flex justify-between sm:space-x-8 text-indigo-500 font-semibold pt-1">
+                <div onClick={(e) => copyToClipBoard("thisiswaleedhere@gmail.com")} className="cursor-pointer">EMAIL</div>
+                <div><a href="https://github.com/thisiswaleedhere" target="_blank" rel="noreferrer">GITHUB</a></div>
+            </div>
+
         </footer>
     )
 }
