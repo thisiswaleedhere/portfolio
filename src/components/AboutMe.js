becb14617fd6c228a637795c7165d27bcf5ceb8d
thisiswaleedhere <thisiswaleedhere@gmail.com>
2022-09-19 13:58:45 +0400

    code organise

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -14,12 +14,14 @@ const AboutMe = () => {
             <div className="xl:max-w-2xl font-base tracking-wide text-gray-300 text-center xl:text-left z-10">
                 <AnimationOnScroll animateIn="animate-fade-in-left">
                     <p className="pb-8">Graduated as a Mechanical Engineer to later find my passion for Frontend Web Development and Design.
-                    </p></AnimationOnScroll>
+                    </p>
+                </AnimationOnScroll>
                 <AnimationOnScroll animateIn="animate-fade-in-left">
                     <p className="pb-8">
                         With an affinity towards beautiful designs and a keen eye for details acquired over the years I was able to use the resources in front of me
                         to learn and thereby design responsive products and build exciting interfaces that ensure great user experiences.
-                    </p></AnimationOnScroll>
+                    </p>
+                </AnimationOnScroll>
                 <AnimationOnScroll animateIn="animate-fade-in-left">
                     <p className="pb-8">If it's not a code editor that is running on my device then it is probably a movie or a football game that keeps me glued to the screen. </p>
                 </AnimationOnScroll>
