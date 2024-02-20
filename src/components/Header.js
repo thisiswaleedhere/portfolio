37185e9cd9320e9360d551631fcfb1c7911dcd9c
waleed-industrielleads <waleed@industrielleads.com>
2024-02-20 06:30:08 +0400

    Made Header Responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,33 +7,24 @@ const Header = ({ setScroll }) => {
     const handleClick = (num) => { setScroll(num); }
 
     return (
-        <header className="relative text-gray-200 font-quicksand px-10 sm:px-16 md:px-28 pt-8 xl:pt-6 pb-16 sm:pb-24 md:pb-32 mx-auto mt-12 flex-col justify-around max-w-9xl min-w-[350px] z-10">
-
-
-            {/* <div className="flex-col justify-center xl:justify-between xl:flex xl:flex-row-reverse xl:py-12"> */}
-
-
-            {/* <img src={MyHero} alt="Hero" className="relative h-36 sm:h-60 xl:h-72 mx-auto my-auto z-10" /> */}
+        <header className="relative text-gray-200 font-quicksand px-10 sm:px-16 md:px-28 pt-8 xl:pt-6 pb-16 sm:pb-24 md:pb-32 mx-auto lg:mt-12 
+        flex-col justify-around max-w-9xl min-w-[350px] z-10">
 
 
 
             <div className="my-auto z-10 xl:pr-8">
 
-                {/* <h2 className="z-10 py-6 text-lg sm:text-2xl md:text-2xl mx-auto text-center xl:text-right sm:leading-relaxed md:leading-relaxed font-light max-w-2xl">
-                    I'm a self taught developer who loves building beautiful user interfaces, web applications and everything in between.  flex-col sm:flex sm:flex-row sm:flex-wrap 
-                </h2> */}
+                <div className="pt-2 text-xl sm:text-3xl lg:text-7xl text-center text-gray-200 font-quicksand tracking-widest">
 
-                <div className="pt-2 text-base sm:text-xl md:text-2xl lg:text-7xl text-center text-gray-200 font-quicksand tracking-widest  justify-center animate-fade-in-down delay-[2000ms]">
-
-                    <div className="text-green-300 px-3 sm:pt-1 text-base md:text-xl lg:text-4xl font-caveat">I'm a</div>
-                    <p>PRODUCT DEVELOPER,</p> <div className="pt-2"> FRONTEND SYSTEMS ENGINEER</div>
-                    <div className="text-green-300 px-3 sm:pt-1 text-base md:text-xl lg:text-4xl font-caveat">and I also do a bit of</div>
-                    <div className="pt-2">DESIGNING</div>
+                    <div className="text-green-300 px-3 sm:pt-1 text-xl sm:text-3xl lg:text-4xl font-caveat">I'm a</div>
+                    <p>PRODUCT DEVELOPER,</p> <div className="pt-1 lg:pt-2"> FRONTEND SYSTEMS ENGINEER</div>
+                    <div className="text-green-300 px-3 sm:pt-1 text-xl sm:text-3xl lg:text-4xl font-caveat">and I also do a bit of</div>
+                    <div className="pt-1 lg:pt-2">DESIGNING</div>
 
                 </div>
 
 
-                <div className="md:my-6 sm:mt-3 md:mt-16 text-xl sm:text-2xl md:text-3xl flex justify-center">
+                <div className="md:my-6 mt-12 sm:mt-8 md:mt-16 text-xl sm:text-2xl md:text-3xl flex flex-col gap-2 sm:gap-4 lg:gap-0 w-32 mx-auto sm:flex-row sm:justify-center">
 
                     <button className="bg-transparent h-10 w-32 sm:w-[136px] md:w-40 md:h-14 px-4 md:py-2 shadow-xl border-2 transition ease-in-out hover:scale-110 border-green-300 hover:bg-green-300 text-green-300 hover:text-black"
                         onClick={() => handleClick(1)}  >
@@ -47,7 +38,7 @@ const Header = ({ setScroll }) => {
 
                     <button className="bg-transparent h-10 w-32 sm:w-[136px] md:w-40 md:h-14 px-4 md:py-2 shadow-xl border-2 transition ease-in-out hover:scale-110 border-green-300 hover:bg-green-300 text-green-300 hover:text-black"
                         onClick={() => handleClick(3)}  >
-                        <div className="font-caveat">Get in touch</div>
+                        <div className="font-caveat">Get&nbsp;in&nbsp;touch</div>
                     </button>
 
                 </div>
--- src/components/Title.js
+++ src/components/Title.js
@@ -2,7 +2,6 @@ import React from "react";
 import { ReactComponent as LogoMW } from "../images/LOGOMW.svg";
 
 
-
 const Title = () => {
     return (
         <div className="relative max-w-8xl min-w-[350px] z-10">
@@ -10,20 +9,10 @@ const Title = () => {
             <div className="flex mx-auto justify-center">
 
                 {/* <LogoMW className="w-6 sm:w-10 pt-6 md:pt-8 xl:pt-12 fill-[#6e9f6f8a] animate-fade-in-down" /> */}
-                <h1 className="pt-6 md:pt-10 xl:pt-12 text-3xl sm:text-5xl md:text-6xl lg:text-4xl text-gray-500 hover:text-yellow-500 text-center uppercase font-caveat tracking-widest sm:tracking-[.16em] ">mohamed waleed</h1>
+                <h1 className="pt-6 md:pt-10 xl:pt-12 text-xl sm:text-3xl md:text-4xl text-gray-500 hover:text-yellow-500
+                 text-center uppercase font-caveat tracking-widest sm:tracking-[.16em] ">mohamed waleed</h1>
 
             </div>
-
-
-
-            {/* <div className="pt-2 text-base sm:text-xl md:text-2xl lg:text-3xl text-center text-gray-400 font-quicksand tracking-widest flex-col sm:flex sm:flex-row sm:flex-wrap justify-center animate-fade-in-down delay-[2000ms]">
-                <div>PRODUCT / FRONTEND SYSTEMS DEVELOPER</div>
-                <div className="flex justify-center">
-                    <div className="text-green-300 px-3 sm:pt-1 text-base md:text-xl lg:text-2xl font-caveat">and also does a bit of</div>
-                    <div>DESIGNING</div>
-                </div>
-            </div> */}
-
 
         </div>
     )
